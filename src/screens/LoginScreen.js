9597321095c05ddc2a975970c98a3aa5a7bbf16d
parth-brilworks <parth.c@brilworks.com>
2023-03-16 16:16:12 +0530

    login demo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { SafeAreaView, Text ,View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import styles from "../styles/loginScreenStyle";
 
-
-const LoginScreen = () =>{
-    return(
-    <SafeAreaView style={{flex:1}}>
-        <View style={{}}>
-            <Text>
-                Welcome To Smart Texi
-            </Text>
-        </View>
+const LoginScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.view}>
+        <Text style={styles.textHeading}>Login Here</Text>
+        <TextInput
+          placeholder="Username or Email"
+          keyboardType={"email-address"}
+          style={styles.inputfield}
+        />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry={true}
+          style={styles.inputfield}
+        />
+      </View>
     </SafeAreaView>
-)}
+  );
+};
 
 export default LoginScreen;
---
+++ src/styles/loginScreenStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  view: {
+    marginTop: "40%",
+    margin: 20,
+  },
+  textHeading: {
+    textAlign: "auto",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    fontSize: 40,
+  },
+  inputfield: {
+    height: 40,
+    borderBottomColor: "#f5ad00",
+    borderBottomWidth: 1,
+    paddingVertical: 10,
+    marginRight: 30,
+  },
+});
+
+export default styles;
