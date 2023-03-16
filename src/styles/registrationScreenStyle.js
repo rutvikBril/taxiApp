73125abccee050a6d2e65956a6d9c53434ff0e91
rutvikBril <rutvik.t@brilworks.com>
2023-03-16 16:15:15 +0530

    registrationScreen

---
+++ src/styles/registrationScreenStyle.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  singUpTextView: {
+    margin: 20,
+  },
+  singUpText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#00000",
+  },
+  formView: {
+    margin: 20,
+  },
+  labelTextStyle: {
+    marginTop: 20,
+    fontSize: 15,
+    color: "#000000",
+  },
+  textInputStyle: {
+    paddingVertical: 10,
+    marginRight: 30,
+    color: "#000000",
+    borderBottomWidth: 2,
+    borderBottomColor: "#f5ad00",
+  },
+  singUpButton: {
+    alignItems: "center",
+    marginTop: "20%",
+    margin: 20,
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 20,
+    backgroundColor: "#f5ad00",
+  },
+});
+export default style;
